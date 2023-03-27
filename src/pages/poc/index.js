72c715e92843f9952d844e2b783f4cc84b349070
haresh-samnani <haresh.samnani@tntra.io>
2023-03-27 11:31:56 +0530

    Added POC of rsuite table with inline editing

---
+++ src/pages/poc/index.js
@@ -0,0 +1,7 @@
+import POC from './poc';
+
+const POCPage = () => {
+  return <POC />;
+};
+
+export default POCPage;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import { 
-  BrowserRouter, 
-  Navigate, 
-  Route, 
-  Routes 
-} from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Dashboard from '../pages/dashboard';
 import Login from '../pages/login';
 import PublicRoute from './publicRoute';
@@ -12,16 +7,18 @@ import PublicRoute from './publicRoute';
 import UserRoute from './userRoute';
 import Profile from '../pages/profile';
 import Signup from '../pages/signup';
+import POCPage from '../pages/poc';
 
 const routerMapper = [
   {
     path: '/login',
-    component: Login, 
+    component: Login,
     routeComponent: PublicRoute,
-  },{
-      path: '/signup',
-      component: Signup,
-      routeComponent: PublicRoute,
+  },
+  {
+    path: '/signup',
+    component: Signup,
+    routeComponent: PublicRoute,
   },
   {
     path: '/dashboard',
@@ -32,6 +29,11 @@ const routerMapper = [
     path: '/profile',
     component: Profile,
     routeComponent: UserRoute,
+  },
+  {
+    path: '/poc',
+    component: POCPage,
+    routeComponent: PublicRoute,
   },
 ];
 
@@ -44,7 +46,9 @@ const RouterConfig = () => {
           path="/*"
           element={<Navigate to={isAuthenticated ? '/login' : '/login'} />}
         />
-        {routerMapper.map(({ component, path, routeComponent }) => routeComponent({ path, isAuthenticated, component }))}
+        {routerMapper.map(({ component, path, routeComponent }) =>
+          routeComponent({ path, isAuthenticated, component }),
+        )}
       </Routes>
     </BrowserRouter>
   );
